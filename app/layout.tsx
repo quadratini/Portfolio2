8d91209e0b36f65d87c8a175926eab85487f4d59
Ronny Ritprasert <ronnyrit@gmail.com>
2023-12-30 14:28:55 -0800

    add simple navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
-import Image from 'next/image'
+import '/components/navbar.css'
+
+function ProjectHolder() {
+  return (
+    <div>
+      <p>A giant square</p>
+    </div>
+  );
+}
+
+function NavBar() {
+  return (
+    <div className="navbar">
+      <a className="navbarItem">Home</a>
+      <a className="navbarItem">About</a>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
-    <>
-      Hello
-    </>
-  )
-}
+    <div>
+      <NavBar />
+      <p>Hello</p>
+      <ProjectHolder />
+    </div>
+  );
+}                                       
