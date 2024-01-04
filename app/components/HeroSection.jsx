be91afc94e6f0becaa1920bc2ba8d85f27b661f6
Ronny Ritprasert <ronnyrit@gmail.com>
2024-01-03 18:56:02 -0800

    add button styling

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -14,8 +14,10 @@ const HeroSection = () => {
               you... hihowareyahihowareya.
             </p>
             <div>
-                <button>Hire Me</button>
-                <button>Download CV</button>
+              <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">Hire Me</button>
+              <button className="px-6 py-3 rounded-full mr-4 bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
+                Download CV
+              </button>
             </div>
           </div>
           <div className="col-span-5 place-self-center mt-4 lg:mt-0">
