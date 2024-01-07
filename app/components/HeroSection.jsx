d26a864805a1fd20c1c251f79b6d82ce44560d99
Ronny Ritprasert <ronnyrit@gmail.com>
2024-01-06 20:13:02 -0800

    update icon size

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
     return (
       <section>
         <div className="grid grid-cols-1 sm:grid-cols-12">
-          <div className="col-span-7 place-self-center text-center sm:text-left">
+          <div className="col-span-7 place-self-center sm:text-left">
             <h1 className="text-white mb-4 text-4xl sm:text-5xl font-extrabold">
               <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700">
                 Hi! I&apos;m Ronny
@@ -16,7 +16,7 @@ const HeroSection = () => {
               <TypeAnimation
                 sequence={["Web Developer", 1000, "Software Engineer", 1000]}
                 wrapper="span"
-                speed={50}
+                speed={45}
                 repeat={Infinity}
               />
             </h1>
@@ -33,13 +33,13 @@ const HeroSection = () => {
             </div>
           </div>
           <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-            <div className="rounded-full bg-[#181818] w-[200px] h-[200px] lg:w-[400px] lg:h-[400px] relative">
+            <div className="rounded-full bg-[#181818] w-[180px] h-[180px] lg:w-[360px] lg:h-[360px] relative">
               <Image
                 src="/images/hero-image.png"
                 alt="hero image"
                 className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                width={300}
-                height={300}
+                width={275}
+                height={275}
               />
             </div>
           </div>
