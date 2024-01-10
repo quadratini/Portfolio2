15d402fc4b205e8423d14c0b08e3350f5ec5a62f
Ronny Ritprasert <ronnyrit@gmail.com>
2024-01-09 20:05:36 -0800

    Add projects section

--- app/components/ProjectSection.jsx
+++ app/components/ProjectSection.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import ProjectCard from "./ProjectCard";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "React Portfolio Website",
+    description: "My React project, the one you're looking at...",
+    image: "/images/projects/1.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/quadratini/Portfolio2",
+    previewUrl: "/",
+  },
+];
 
 const ProjectsSection = () => {
-    return (
-        <>
-        <h2>My Projects</h2><div></div>
-        </>
-    )
-}
+  return (
+    <>
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-4">
+        My Projects
+      </h2>
+      <div>
+        {projectsData.map((project) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            imgUrl={project.image}
+            gitUrl={project.gitUrl}
+            previewUrl={project.previewUrl}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default ProjectsSection;
