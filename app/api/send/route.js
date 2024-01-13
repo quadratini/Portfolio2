c361af24388617a5281ae8cd6577e429fef80b5b
Ronny Ritprasert <ronnyrit@gmail.com>
2024-01-12 23:54:20 -0800

    make site build

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,19 +1,19 @@
 // import { EmailTemplate } from '../../../components/EmailTemplate';
-import { Resend } from 'resend';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
-  try {
-    const data = await resend.emails.send({
-      from: 'Ronny <ronnyrit@gmail.com>',
-      to: ['ronnyrit@gmail.com'],
-      subject: 'Hello world',
-      react: <></>,
-    });
+// export async function POST() {
+//   try {
+//     const data = await resend.emails.send({
+//       from: 'Ronny <ronnyrit@gmail.com>',
+//       to: ['ronnyrit@gmail.com'],
+//       subject: 'Hello world',
+//       react: <></>,
+//     });
 
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error });
-  }
-}
+//     return Response.json(data);
+//   } catch (error) {
+//     return Response.json({ error });
+//   }
+// }
