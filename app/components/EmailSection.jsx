8212b80bceddebcbce3ee15fc7e061f8a88d3a1f
Ronny Ritprasert <ronnyrit@gmail.com>
2024-01-13 23:45:23 -0800

    added code (another cheat day)

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const EmailSection = () => {
-    return <section className="grid md:gird-cols-2 ">EmailSection</section>;
-}
+  return (
+    <section className="grid md:gird-cols-2  my-12 mmd:my-12 py-24 gap-4">
+      Contact me
+      <div>
+        <h5 className="text-xl font-bold text-white my-2"></h5>
+      </div>
+    </section>
+  );
+};
 
 export default EmailSection;
