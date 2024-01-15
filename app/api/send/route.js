5c499c15592a7ad6177d7016398c2808780fc2df
Ronny Ritprasert <ronnyrit@gmail.com>
2024-01-14 17:57:50 -0800

    added email section

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,19 +1,23 @@
 // import { EmailTemplate } from '../../../components/EmailTemplate';
-// import { Resend } from 'resend';
+import { Resend } from "resend";
 
-// const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-// export async function POST() {
-//   try {
-//     const data = await resend.emails.send({
-//       from: 'Ronny <ronnyrit@gmail.com>',
-//       to: ['ronnyrit@gmail.com'],
-//       subject: 'Hello world',
-//       react: <></>,
-//     });
+export async function POST() {
+  try {
+    const data = await resend.emails.send({
+      from: "Ronny <ronnyrit@gmail.com>",
+      to: ["ronnyrit@gmail.com"],
+      subject: "Hello world",
+      react: (
+        <>
+          <p>Body goes here</p>
+        </>
+      ),
+    });
 
-//     return Response.json(data);
-//   } catch (error) {
-//     return Response.json({ error });
-//   }
-// }
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error });
+  }
+}
--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -1,11 +1,44 @@
 import React from "react";
+import GitHubIcon from "../../public/github-mark-white.svg";
+import LinkedinIcon from "../../public/linkedin-icon.png";
+import Link from "next/link";
+import Image from "next/image";
 
 const EmailSection = () => {
   return (
-    <section className="grid md:gird-cols-2  my-12 mmd:my-12 py-24 gap-4">
-      Contact me
+    <section className="grid md:gird-cols-2 my-12 mmd:my-12 py-24 gap-4">
       <div>
-        <h5 className="text-xl font-bold text-white my-2"></h5>
+        <h5 className="text-xl font-bold text-white my-2">Contact me</h5>
+      </div>
+      <p className="text-[#ADB7BE] mb-4 max-w-md">
+        {" "}
+        Want to chat?
+      </p>
+      <div>
+        <form className="flex flex-col gap-4">
+          <label
+            htmlFor="email"
+            type="email"
+            className="text-white block mb-2 text-sm font-medium"
+          >
+            Your email
+          </label>
+          <input
+            type="email"
+            id="email"
+            required
+            className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+            placeholder="youremail@gmail.com"
+          />
+        </form>
+      </div>
+      <div className="socials flex flex-row gap-2">
+        <Link href="https://github.com/quadratini">
+          <Image src={GitHubIcon} alt="Github Icon" className="h-10 w-10" />
+        </Link>
+        <Link href="https://www.linkedin.com/in/ronny-ritprasert-35a82b183/">
+          <Image src={LinkedinIcon} alt="LinkedIn Icon" className="h-10 w-11" />
+        </Link>
       </div>
     </section>
   );
